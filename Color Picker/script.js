47eff6cc2aeae04913dbd680a7980b3dce737b0a
Hareesh <you@example.com>
2023-12-14 11:39:29 +0530

    Initial commit

---
+++ Color Picker/script.js
@@ -0,0 +1,29 @@
+let containerEl = document.getElementById("container");
+let secdivEl = document.getElementById("secdiv");
+let tdiv = document.getElementById("tdiv");
+let hexcodeEl = document.getElementById("hexCode");
+
+function green() {
+  containerEl.style.backgroundColor = "green";
+  secdivEl.style.backgroundColor = "green";
+  tdiv.style.backgroundColor = "green";
+  hexcodeEl.textContent = "green";
+}
+function red() {
+  containerEl.style.backgroundColor = "red";
+  secdivEl.style.backgroundColor = "red";
+  tdiv.style.backgroundColor = "red";
+  hexcodeEl.textContent = "red";
+}
+function blue() {
+  containerEl.style.backgroundColor = "blue";
+  secdivEl.style.backgroundColor = "blue";
+  tdiv.style.backgroundColor = "blue";
+  hexcodeEl.textContent = "blue";
+}
+function brown() {
+  containerEl.style.backgroundColor = "brown";
+  secdivEl.style.backgroundColor = "brown";
+  tdiv.style.backgroundColor = "brown";
+  hexcodeEl.textContent = "brown";
+}
